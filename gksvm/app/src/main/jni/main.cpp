25ab0e75750f15de1108115a14d9f8bfa8b66bcf
leepaul009 <leepaul009@hotmail.com>
2017-08-24 15:47:27 +0800

    Update main.cpp

--- gksvm/app/src/main/jni/main.cpp
+++ gksvm/app/src/main/jni/main.cpp
@@ -1,7 +1,3 @@
-//
-// Created by lee on 2016-09-02.
-//
-
 #include <jni.h>
 #include <string.h>
 #include <android/log.h>
@@ -10,25 +6,20 @@
 #include <stdlib.h>
 #include <vector>
 #include "common.h"
-#include "test.h"
+#include "algorithm.h"
 #include <CL/cl.h>
 #include <malloc.h>
 
 #include "com_example_gksvm_MainActivity.h"
 
-
-
 JNIEXPORT jstring JNICALL Java_com_example_gksvm_MainActivity_jnifunc(JNIEnv *env, jobject obj){
-
-    char buffer[1024] = "hello,jni!";
-    //sprintf(buffer, "%d\n");
+    char buffer[1024] = "Run GPU-SVM-kNN!";
     return env->NewStringUTF(buffer);
-    //return (*env)->NewStringUTF(env, "Hello World! I am Native interface");
 }
 
 JNIEXPORT jstring JNICALL Java_com_example_gksvm_MainActivity_jniSvmRes
   (JNIEnv *env, jobject obj, jstring cmdIn){
-    test::main();
+    algorithm::main();
     char buffer[1024];
     return env->NewStringUTF(buffer);
 }
